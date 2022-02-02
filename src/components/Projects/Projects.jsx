e713bbc8b19a9acdd21aa561d3b1b0d6cb6a1d23
Gianluca Fuoco <gianluca_1227@hotmail.com>
2022-02-02 18:28:51 -0500

    Added links to project cards

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -7,6 +7,22 @@ const Projects = () => {
   A MERN stack application demonstrating a shopping website. The site contains users, shopping carts, and checkout with Stripe.
   `;
 
+  const socialMediaDesc = `
+  A social media website built with Ruby on Rails. It features likes, posts, comments, users, profiles, uploading a photo with a caption, creating an account, password resets, friend requests, etc.
+  `;
+
+  const battleShipDesc = `
+  An implementation of battleship in the browser. Made with Javascript and Webpack.
+  `;
+
+  const rubyChessDesc = `
+  A command-line game of chess. Built with Ruby.
+  `;
+
+  const dogWebsiteDesc = `
+  An example of a dog breeder website for the English Bulldog.
+  `;
+
   return (
     <section className="projects-container">
       <h2 className="projects-title">Projects</h2>
@@ -19,25 +35,32 @@ const Projects = () => {
           name="Mern Shopping App"
         />
         <ProjectCard
-          link={""}
+          link={"https://thepeoplenetwork.herokuapp.com"}
           imageObject={{ source: "", alt: "example" }}
-          description={"desc"}
-          github={""}
+          description={socialMediaDesc}
+          github={"https://github.com/geefuoco/the_people_network"}
           name="Social Media Site"
         />
         <ProjectCard
-          link={""}
+          link={"https://battleshipjavascript.netlify.app"}
           imageObject={{ source: "", alt: "example" }}
-          description={"desc"}
-          github={""}
+          description={battleShipDesc}
+          github={"https://github.com/battleshipjs"}
           name="Battleship"
         />
         <ProjectCard
-          link={""}
+          link={"https://replit.com/@geefuoco/rubychess#"}
           imageObject={{ source: "", alt: "example" }}
-          description={"desc"}
-          github={""}
+          description={rubyChessDesc}
+          github={"https://github.com/geefuoco/ruby_chess"}
           name="Chess"
+        />
+        <ProjectCard
+          link={"https://bulldogwebsite.netlify.app/"}
+          imageObject={{ source: "", alt: "example" }}
+          description={dogWebsiteDesc}
+          github={"https://github.com/geefuoco/dog-website"}
+          name="Example Dog Website"
         />
       </div>
     </section>
